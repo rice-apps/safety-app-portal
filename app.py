5ae289656071c2fb653847caa69fac7699d85fa0
jeffreyxiong <jeffxiong017@gmail.com>
2017-10-18 22:26:17 -0500

    Initial Flask

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
 from flask import Flask, request, g, render_template, jsonify
 from flask_socketio import SocketIO
-import config
+# import config
 
 # Flask
 app = Flask(__name__)
-app.config.from_object("config")
+# app.config.from_object("config")
 
 # SocketIO
 socketio = SocketIO(app)
 
 
 ###############################################################################
-# Database Operations
+# Routing
 ###############################################################################
 
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+
+
+if __name__ == '__main__':
+    app.run()
 
